54a8448fabccf6e7edc1ae60c58ac31ec54d4c6f
kesel <a.kiselyoff@gmail.com>
2022-12-11 20:17:05 +0200

    checking oop about modifictors

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,90 @@
 "use strict";
-const x = 7;
-class Person {
+class Vehicle {
+    constructor(engineType, type, speed, color) {
+        this.engineType = engineType;
+        this.type = type;
+        this.speed = speed;
+        this.color = color;
+        Vehicle.count += 1;
+    }
+    showCount() {
+        console.log('Vehicle serial number: ' + Vehicle.count);
+    }
+    showEngineType() {
+        console.log('Engine Type: ' + this.engineType);
+    }
+    showType() {
+        console.log('Type: ' + this.type);
+    }
+    showSpeed() {
+        console.log('Speed: ' + this.speed);
+    }
+    showColor() {
+        console.log('Color: ' + this.color);
+    }
+    speedTuning(speed) {
+        this.speed = speed;
+        console.log('Speed increased to: ' + this.speed);
+    }
+    colorChange(color) {
+        this.color = color;
+        console.log('Color changed to: ' + this.color);
+    }
 }
-console.log('hello');
+Vehicle.count = 0;
+class TuningVehicle extends Vehicle {
+    constructor(spoiler, nitro, lightning) {
+        super('fuel', 'bus', 150, 'yellow');
+        this.spoiler = spoiler;
+        this.nitro = nitro;
+        this.lightning = lightning;
+    }
+    addSpoiler() {
+        this.spoiler = true;
+        console.log('+ Spoiler');
+    }
+    checkSpoiler() {
+        this.spoiler === true ? console.log('Spoiler +') : console.log('Spoiler -');
+    }
+    addNitro() {
+        this.nitro = true;
+        console.log('+ Nitro');
+    }
+    checkNitro() {
+        this.nitro === true ? console.log('Nitrogen +') : console.log('Nitrogen -');
+    }
+    addLightning() {
+        this.lightning = true;
+        console.log('+ Lightning');
+    }
+    checkLightning() {
+        this.lightning === true
+            ? console.log('Lightning +')
+            : console.log('Lightning -');
+    }
+    showStartOptions() {
+        super.showCount();
+        super.showEngineType();
+        super.showType();
+        super.showSpeed();
+        super.showColor();
+    }
+    checkTuning() {
+        this.checkSpoiler();
+        this.checkNitro();
+        this.checkLightning();
+    }
+    fullTuning(speed, color) {
+        this.addSpoiler();
+        this.addNitro();
+        this.addLightning();
+        super.speedTuning(speed);
+        super.colorChange(color);
+        console.log('Your car is full tuned');
+    }
+    showCharacteristics() {
+        this.showStartOptions();
+        this.checkTuning();
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,94 @@
-const x: number = 7;
-class Person {}
+class Vehicle {
+  private static count: number = 0;
 
-console.log('hello');
+  constructor(
+    private engineType: 'electro' | 'fuel',
+    private type: 'SUV' | 'sedan' | 'hatchback' | 'bus' | 'vane' | 'universal',
+    private speed: number,
+    private color: string
+  ) {
+    Vehicle.count += 1;
+  }
+
+  showCount() {
+    console.log('Vehicle serial number: ' + Vehicle.count);
+  }
+  showEngineType() {
+    console.log('Engine Type: ' + this.engineType);
+  }
+  showType() {
+    console.log('Type: ' + this.type);
+  }
+  showSpeed() {
+    console.log('Speed: ' + this.speed);
+  }
+  showColor() {
+    console.log('Color: ' + this.color);
+  }
+  speedTuning(speed: number) {
+    this.speed = speed;
+    console.log('Speed increased to: ' + this.speed);
+  }
+  colorChange(color: string) {
+    this.color = color;
+    console.log('Color changed to: ' + this.color);
+  }
+}
+
+class TuningVehicle extends Vehicle {
+  constructor(
+    private spoiler: boolean,
+    private nitro: boolean,
+    private lightning: boolean
+  ) {
+    super('fuel', 'bus', 150, 'yellow');
+  }
+  addSpoiler() {
+    this.spoiler = true;
+    console.log('+ Spoiler');
+  }
+  checkSpoiler() {
+    this.spoiler === true ? console.log('Spoiler +') : console.log('Spoiler -');
+  }
+  addNitro() {
+    this.nitro = true;
+    console.log('+ Nitro');
+  }
+  checkNitro() {
+    this.nitro === true ? console.log('Nitrogen +') : console.log('Nitrogen -');
+  }
+  addLightning() {
+    this.lightning = true;
+    console.log('+ Lightning');
+  }
+  checkLightning() {
+    this.lightning === true
+      ? console.log('Lightning +')
+      : console.log('Lightning -');
+  }
+  showStartOptions() {
+    super.showCount();
+    super.showEngineType();
+    super.showType();
+    super.showSpeed();
+    super.showColor();
+  }
+  checkTuning() {
+    this.checkSpoiler();
+    this.checkNitro();
+    this.checkLightning();
+  }
+  fullTuning(speed: number, color: string) {
+    this.addSpoiler();
+    this.addNitro();
+    this.addLightning();
+    super.speedTuning(speed);
+    super.colorChange(color);
+    console.log('Your car is full tuned');
+  }
+
+  showCharacteristics() {
+    this.showStartOptions();
+    this.checkTuning();
+  }
+}
