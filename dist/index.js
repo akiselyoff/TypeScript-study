ba8570b10ed2f158fcfacf18f79c10e05dafeb9e
kesel <a.kiselyoff@gmail.com>
2022-12-21 00:07:14 +0200

    intersection types and type guards

--- dist/index.js
+++ dist/index.js
@@ -1,108 +1,18 @@
 "use strict";
-class Vehicle {
-    constructor(engineType, type, speed, color) {
-        this.engineType = engineType;
-        this.type = type;
-        this.speed = speed;
-        this.color = color;
-        this.options = [];
-        Vehicle.count += 1;
+const user = {
+    name: 'Max',
+    age: 25,
+    password: 'qwerty',
+};
+function showFields(obj) {
+    console.log(obj.name);
+    if ('age' in obj) {
+        console.log('Age: ' + obj.age + ' years old');
     }
-    showCount() {
-        console.log('Vehicle serial number: ' + Vehicle.count);
-    }
-    showEngineType() {
-        console.log('Engine Type: ' + this.engineType);
-    }
-    showType() {
-        console.log('Type: ' + this.type);
-    }
-    showSpeed() {
-        console.log('Speed: ' + this.speed);
-    }
-    showColor() {
-        console.log('Color: ' + this.color);
-    }
-    addOption(option) {
-        this.options.push(option);
-    }
-    speedTuning(speed) {
-        this.speed = speed;
-        console.log('Speed increased to: ' + this.speed);
-    }
-    colorChange(color) {
-        this.color = color;
-        console.log('Color changed to: ' + this.color);
-    }
-    showStartOptions() {
-        this.showCount();
-        this.showEngineType();
-        this.showType();
-        this.showSpeed();
-        this.showColor();
+    if ('password' in obj) {
+        console.log('Password is: ' + obj.password);
     }
 }
-Vehicle.count = 0;
-class TuningVehicle extends Vehicle {
-    constructor(spoiler, nitro, lightning) {
-        super('fuel', 'bus', 150, 'yellow');
-        this.spoiler = spoiler;
-        this.nitro = nitro;
-        this.lightning = lightning;
-    }
-    addSpoiler() {
-        this.spoiler = true;
-        console.log('+ Spoiler');
-    }
-    checkSpoiler() {
-        this.spoiler === true ? console.log('Spoiler +') : console.log('Spoiler -');
-    }
-    addNitro() {
-        this.nitro = true;
-        console.log('+ Nitro');
-    }
-    checkNitro() {
-        this.nitro === true ? console.log('Nitrogen +') : console.log('Nitrogen -');
-    }
-    addLightning() {
-        this.lightning = true;
-        console.log('+ Lightning');
-    }
-    checkLightning() {
-        this.lightning === true
-            ? console.log('Lightning +')
-            : console.log('Lightning -');
-    }
-    checkTuning() {
-        this.checkSpoiler();
-        this.checkNitro();
-        this.checkLightning();
-    }
-    fullTuning(speed, color) {
-        this.addSpoiler();
-        this.addNitro();
-        this.addLightning();
-        super.speedTuning(speed);
-        super.colorChange(color);
-        console.log('Your car is full tuned');
-    }
-    showCharacteristics() {
-        super.showStartOptions();
-        this.checkTuning();
-    }
-    startEngine() {
-        console.log('Engine was start!');
-        return true;
-    }
-}
-class Driver {
-    constructor(name, age, driverCategory) {
-        this.name = name;
-        this.age = age;
-        this.driverCategory = driverCategory;
-    }
-    greet(phrase) {
-        console.log(this.name + ' said: ' + phrase);
-    }
-}
+showFields({ name: 'Mary', age: 21 });
+showFields({ name: 'Nik', password: '12345' });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,153 +1,44 @@
-abstract class Vehicle {
-  private static count: number = 0;
-  private options: string[] = [];
+type User = {
+  name: string;
+  age: number;
+};
 
-  constructor(
-    private engineType: 'electro' | 'fuel',
-    private type: 'SUV' | 'sedan' | 'hatchback' | 'bus' | 'vane' | 'universal',
-    private speed: number,
-    private color: string
-  ) {
-    Vehicle.count += 1;
-  }
-
-  showCount() {
-    console.log('Vehicle serial number: ' + Vehicle.count);
-  }
-  showEngineType() {
-    console.log('Engine Type: ' + this.engineType);
-  }
-  showType() {
-    console.log('Type: ' + this.type);
-  }
-  showSpeed() {
-    console.log('Speed: ' + this.speed);
-  }
-  showColor() {
-    console.log('Color: ' + this.color);
-  }
-  addOption(option: string) {
-    this.options.push(option);
-  }
-  speedTuning(speed: number) {
-    this.speed = speed;
-    console.log('Speed increased to: ' + this.speed);
-  }
-  colorChange(color: string) {
-    this.color = color;
-    console.log('Color changed to: ' + this.color);
-  }
-  showStartOptions() {
-    this.showCount();
-    this.showEngineType();
-    this.showType();
-    this.showSpeed();
-    this.showColor();
-  }
-  abstract startEngine(): boolean;
-}
-
-class TuningVehicle extends Vehicle {
-  constructor(
-    private spoiler: boolean,
-    private nitro: boolean,
-    private lightning: boolean
-  ) {
-    super('fuel', 'bus', 150, 'yellow');
-  }
-  addSpoiler() {
-    this.spoiler = true;
-    console.log('+ Spoiler');
-  }
-  checkSpoiler() {
-    this.spoiler === true ? console.log('Spoiler +') : console.log('Spoiler -');
-  }
-  addNitro() {
-    this.nitro = true;
-    console.log('+ Nitro');
-  }
-  checkNitro() {
-    this.nitro === true ? console.log('Nitrogen +') : console.log('Nitrogen -');
-  }
-  addLightning() {
-    this.lightning = true;
-    console.log('+ Lightning');
-  }
-  checkLightning() {
-    this.lightning === true
-      ? console.log('Lightning +')
-      : console.log('Lightning -');
-  }
-
-  checkTuning() {
-    this.checkSpoiler();
-    this.checkNitro();
-    this.checkLightning();
-  }
-  fullTuning(speed: number, color: string) {
-    this.addSpoiler();
-    this.addNitro();
-    this.addLightning();
-    super.speedTuning(speed);
-    super.colorChange(color);
-    console.log('Your car is full tuned');
-  }
-  showCharacteristics() {
-    super.showStartOptions();
-    this.checkTuning();
-  }
-  startEngine() {
-    console.log('Engine was start!');
-    return true;
-  }
-}
-
-interface IPerson {
+interface IUser {
   name: string;
   age: number;
 }
 
-// type IPerson = {//type одно и тоже с interface
-//   name: string;
-//   age: number;
-// }
-
-const driver: IPerson = {
-  name: 'Joe',
-  age: 25,
+type Admin = {
+  name: string;
+  password: string;
 };
 
-interface IDriverLicense extends IPerson {
-  driverCategory: 'A' | 'B' | 'C' | 'D' | 'E';
+interface IAdmin {
+  name: string;
+  password: string;
 }
 
-class Driver implements IDriverLicense {
-  constructor(
-    public name: string,
-    public age: number,
-    public driverCategory: 'A' | 'B' | 'C' | 'D' | 'E'
-  ) {}
+type SuperUser = User & Admin;
 
-  greet(phrase: string): void {
-    console.log(this.name + ' said: ' + phrase);
+interface ISuperUser extends User, Admin {}
+
+const user: SuperUser = {
+  name: 'Max',
+  age: 25,
+  password: 'qwerty',
+};
+
+type UnknownUser = IUser | IAdmin;
+
+function showFields(obj: UnknownUser) {
+  console.log(obj.name);
+  if ('age' in obj) {
+    console.log('Age: ' + obj.age + ' years old');
+  }
+  if ('password' in obj) {
+    console.log('Password is: ' + obj.password);
   }
 }
 
-type AddFuncType = (n1: number, n2: number) => number;
-
-let add1: AddFuncType;
-
-add1 = (n1: number, n2: number) => {
-  return n1 + n2;
-};
-
-//type AddFunc = (n1: number, n2: number) => number;
-interface AddFuncInterface {
-  (n1: number, n2: number): number;
-}
-
-let add2: AddFuncInterface;
-
-add2 = (n1: number, n2: number) => {
-  return n1 + n2;
-};
+showFields({ name: 'Mary', age: 21 });
+showFields({ name: 'Nik', password: '12345' });
