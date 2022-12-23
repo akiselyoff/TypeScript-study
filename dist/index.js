b73ad6f47b82affbb737eee6d86387ee0f855171
kesel <a.kiselyoff@gmail.com>
2022-12-23 17:54:58 +0200

    advanced type done

--- dist/index.js
+++ dist/index.js
@@ -20,4 +20,51 @@ input.value = 'Vika';
 console.log(input.value);
 const input2 = document.getElementById('last_name');
 input2.value = 'Boris';
+const input3 = document.getElementById('myCity');
+if (input3) {
+    input3.value = 'Kyiv';
+}
+const patient = {
+    name: 'Alex',
+    surname: 'Smith',
+    eyeColor: 'Brown',
+};
+const driver = {
+    licence: true,
+    additionalInfo: {
+        info: 'Always drunk',
+    },
+};
+function showDriverAdditionalInfo() {
+    var _a, _b;
+    if ((_a = driver === null || driver === void 0 ? void 0 : driver.additionalInfo) === null || _a === void 0 ? void 0 : _a.info) {
+        console.log('Additional info about driver: ' +
+            ((_b = driver === null || driver === void 0 ? void 0 : driver.additionalInfo) === null || _b === void 0 ? void 0 : _b.info) +
+            'Has driver licence: ' +
+            driver.licence);
+    }
+}
+showDriverAdditionalInfo();
+const userInput = '';
+const store = userInput || 'DEFAULT';
+console.log(store);
+const userInput2 = 0;
+const store2 = userInput2 !== null && userInput2 !== void 0 ? userInput2 : 'DEFAULT';
+console.log(store2);
+function checkUser(name, type) {
+    if (type === 'admin') {
+        return {
+            name,
+            type: 'admin',
+        };
+    }
+    else {
+        return {
+            name,
+            type: 'user',
+        };
+    }
+}
+const simpleUser = checkUser('Nikita', 'user');
+const admin = checkUser('Tonya', 'admin');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,78 @@ if (input3) {
   (input3 as HTMLInputElement).value = 'Kyiv';
 }
 
-const input4 = document.getElementById('age');
-if (input4) {
-  (input4 as HTMLInputElement).value = 25;
+// const input4 = document.getElementById('age');
+// if (input4) {
+//   (input4 as HTMLInputElement).value = 25;
+// }
+
+//Index Properties
+interface IPerson {
+  name: string;
+  [x: string]: string;
 }
+const patient: IPerson = {
+  name: 'Alex',
+  surname: 'Smith',
+  eyeColor: 'Brown',
+};
+//Optional Chaining
+interface IDriver {
+  licence: boolean;
+  additionalInfo?: {
+    info: string;
+  };
+}
+const driver: IDriver = {
+  licence: true,
+  additionalInfo: {
+    info: 'Always drunk',
+  },
+};
+function showDriverAdditionalInfo(): void {
+  if (driver?.additionalInfo?.info) {
+    console.log(
+      'Additional info about driver: ' +
+        driver?.additionalInfo?.info +
+        'Has driver licence: ' +
+        driver.licence
+    );
+  }
+}
+showDriverAdditionalInfo();
+//Nullish Coalescing
+const userInput = '';
+const store = userInput || 'DEFAULT';
+console.log(store); //DEFAULT
+
+const userInput2 = 0;
+const store2 = userInput2 ?? 'DEFAULT';
+console.log(store2); //0
+
+type AdminType = {
+  type: 'admin';
+  name: string;
+};
+
+type UserType = {
+  type: 'user';
+  name: string;
+};
+//Перегрузка операторов (function overloads)
+function checkUser(name: string, type: 'admin'): AdminType;
+function checkUser(name: string, type: 'user'): UserType;
+function checkUser(name: string, type: 'admin' | 'user') {
+  if (type === 'admin') {
+    return {
+      name,
+      type: 'admin',
+    };
+  } else {
+    return {
+      name,
+      type: 'user',
+    };
+  }
+}
+const simpleUser = checkUser('Nikita', 'user');
+const admin = checkUser('Tonya', 'admin');
