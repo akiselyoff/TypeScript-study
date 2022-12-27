cdc22aae2b359bded324d09cb759af77e6330385
kesel <a.kiselyoff@gmail.com>
2022-12-28 01:05:50 +0200

    copy task

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
+function getPromise() {
+  return new Promise(resolve => {
+    resolve(['Text', 50]);
+  });
+}
 
+getPromise().then(data => {
+  console.log(data);
+});
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare(top, bottom): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+function merge(objA, objB) {
+  return Object.assign(objA, objB);
+}
+
+class Component {
+  constructor(public props: T) {}
+}
+
+class Page extends Component {
+  pageInfo() {
+    console.log(this.props.title);
+  }
+}
