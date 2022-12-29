23aa7d5caf8dc54fcd0c24daf032f60229250623
kesel <a.kiselyoff@gmail.com>
2022-12-29 19:53:24 +0200

    promise and pick tasks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-function getPromise() {
+function getPromise(): Promise<Array<string | number>> {
   return new Promise(resolve => {
     resolve(['Text', 50]);
   });
@@ -15,7 +15,10 @@ type AllType = {
   weight: number;
 };
 
-function compare(top, bottom): AllType {
+function compare(
+  top: Pick<AllType, 'name' | 'color'>,
+  bottom: Pick<AllType, 'position' | 'weight'>
+): AllType {
   return {
     name: top.name,
     color: top.color,
