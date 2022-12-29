0c6be8d11b5ebb5c8635be96c9fc2d33c3057ab7
kesel <a.kiselyoff@gmail.com>
2022-12-29 20:02:11 +0200

    compare obj types

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,8 @@ function compare(
   };
 }
 
-function merge(objA, objB) {
-  return Object.assign(objA, objB);
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB);
 }
 
 class Component {
