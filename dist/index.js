9f1665461363257ea89a427d1b02ab351125ddc0
kesel <a.kiselyoff@gmail.com>
2023-01-14 23:16:28 +0200

    save changes

--- dist/index.js
+++ dist/index.js
@@ -1,70 +1,20 @@
 "use strict";
-const user = {
-    name: 'Max',
-    age: 25,
-    password: 'qwerty',
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function showFields(obj) {
-    console.log(obj.name);
-    if ('age' in obj) {
-        console.log('Age: ' + obj.age + ' years old');
-    }
-    if ('password' in obj) {
-        console.log('Password is: ' + obj.password);
-    }
+function Logger(constructor) {
+    console.log('Loging...');
+    console.log(constructor);
 }
-showFields({ name: 'Mary', age: 21 });
-showFields({ name: 'Nik', password: '12345' });
-const input = document.getElementById('first_name');
-input.value = 'Vika';
-console.log(input.value);
-const input2 = document.getElementById('last_name');
-input2.value = 'Boris';
-const input3 = document.getElementById('myCity');
-if (input3) {
-    input3.value = 'Kyiv';
-}
-const patient = {
-    name: 'Alex',
-    surname: 'Smith',
-    eyeColor: 'Brown',
+let Controller = class Controller {
+    constructor() {
+        this.id = 1;
+    }
 };
-const driver = {
-    licence: true,
-    additionalInfo: {
-        info: 'Always drunk',
-    },
-};
-function showDriverAdditionalInfo() {
-    var _a, _b;
-    if ((_a = driver === null || driver === void 0 ? void 0 : driver.additionalInfo) === null || _a === void 0 ? void 0 : _a.info) {
-        console.log('Additional info about driver: ' +
-            ((_b = driver === null || driver === void 0 ? void 0 : driver.additionalInfo) === null || _b === void 0 ? void 0 : _b.info) +
-            'Has driver licence: ' +
-            driver.licence);
-    }
-}
-showDriverAdditionalInfo();
-const userInput = '';
-const store = userInput || 'DEFAULT';
-console.log(store);
-const userInput2 = 0;
-const store2 = userInput2 !== null && userInput2 !== void 0 ? userInput2 : 'DEFAULT';
-console.log(store2);
-function checkUser(name, type) {
-    if (type === 'admin') {
-        return {
-            name,
-            type: 'admin',
-        };
-    }
-    else {
-        return {
-            name,
-            type: 'user',
-        };
-    }
-}
-const simpleUser = checkUser('Nikita', 'user');
-const admin = checkUser('Tonya', 'admin');
+Controller = __decorate([
+    Logger
+], Controller);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,8 @@
-function getPromise(): Promise<Array<string | number>> {
-  return new Promise(resolve => {
-    resolve(['Text', 50]);
-  });
+function Logger(constructor: Function) {
+  console.log('Loging...');
+  console.log(constructor);
 }
-
-getPromise().then(data => {
-  console.log(data);
-});
-
-type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number;
-};
-
-function compare(
-  top: Pick<AllType, 'name' | 'color'>,
-  bottom: Pick<AllType, 'position' | 'weight'>
-): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  };
-}
-
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign({}, objA, objB);
-}
-
-class Component<T> {
-  constructor(public props: T) {}
-}
-
-interface IPage {
-  title: string;
-}
-
-class Page extends Component<IPage> {
-  pageInfo(): void {
-    console.log(this.props.title);
-  }
+@Logger
+class Controller {
+  public id = 1;
 }
